65d20919114d3f1a150af035491f71cb2d4041b1
nhindman <nathaniel.hindman@gmail.com>
2014-04-10 11:47:13 -0400

    returning tweets

--- web.js
+++ web.js
@@ -1,6 +1,82 @@
 var express = require('express');
 var app = express();
+var Twit = require("twit");
+var Q = require('q');
+var request = require("request");
+var EMBEDLY_KEY = '8c394ac3008745d48fe82133e6acc577';
 app.use(express.logger());
+
+var summarize = function(url) {
+  var deferred = Q.defer();
+  request("https://api.embed.ly/1/extract", {
+     qs: {url: url, key: EMBEDLY_KEY}
+   }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      deferred.resolve(JSON.parse(body));
+    } else {
+      deferred.reject(error);
+    }
+   });
+  return deferred.promise;
+};
+
+app.get('/lookup', function(request, response) {
+  var T = new Twit({
+      consumer_key:         'i7lsLu4dOv66EEDzCWEifyoXd',
+      consumer_secret:      '3I7NQsQsjQpCsadhN8t1Ry9Xb3oglUsDTzME6Cc0EGLvrVmZQB',
+      access_token:         '1067293915-gXahWnpOxPZydxkzAO4YzWNfhaSkZp8Nz6KfW4L',
+      access_token_secret:  '1e8Y0rCaLlz9SkdlIqBuMsBbEJvDY1SUb9ZClh6WNkmim',
+  });
+   
+  var tweets = [];
+  var urls = [];
+  var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/
+   
+  T.get('statuses/user_timeline', { 'screen_name' : request.query.username, 'count': 20 }, function(err, reply) {
+    for (i in reply){
+      tweet = reply[i].text;
+   
+      if (urlPattern.test(tweet)){
+        tweets.push(tweet);
+        var urlRegex = /(https?:\/\/[^\s]+)/g;
+        tweet.replace(urlRegex, function(tweet) {
+          console.log("heres a URL",tweet)
+          urls.push(tweet);
+        })
+      }
+    }
+    
+    var summaryPromises = urls.map(function(url) {
+      return summarize(url);
+    });
+
+    Q.allSettled(summaryPromises).then(function(results) {
+      var acceptedSummaries = results.filter(function(result) {
+        return result.state == 'fulfilled';
+      }).map(function(result) {
+        return result.value;
+      }).filter(function(summary) {
+        return !(/twitpic|imgur|instagra/.test(summary.url));
+      }).map(function(summary) {
+        return {
+          url: summary.url,
+          title: summary.title,
+          image: summary.images[0].url,
+          description: summary.description
+        }
+      });
+      console.log('summary Results', acceptedSummaries);
+      if (request.query.callback){
+        var json = JSON.stringify(acceptedSummaries);
+        response.send(request.query.callback+'('+json+')');
+      } else {
+        response.send(acceptedSummaries);
+      } 
+    });
+  });
+  
+  
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -10,3 +86,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
